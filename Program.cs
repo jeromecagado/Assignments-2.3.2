ae54e9524e41f930914fe245ac190b1eb2bbeaca
jeromecagado <jerome.cagado@outlook.com>
2025-07-15 15:53:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignments_2._3._2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the tip calculator\n");
+            while (true)
+            {
+                Console.WriteLine("Please enter the bill total!");
+
+                if (!double.TryParse(Console.ReadLine(), out double billTotal))
+                {
+                    Console.WriteLine("Incorrect input.");
+                    continue;
+                }
+                Console.WriteLine("Please enter the tip percentage:");
+                if (!double.TryParse(Console.ReadLine(), out double tipPercentage))
+                {
+                    Console.WriteLine("Incorrect tip input.");
+                    continue;
+                }
+                TipCalculator.DisplayTotal(billTotal, tipPercentage);
+                Console.WriteLine("\nPress enter to leave.");
+                Console.ReadKey();
+                break;
+            }
+        }
+    }
+}
---
+++ TipCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace Assignments_2._3._2
+{
+    public static class TipCalculator
+    {
+        public static void DisplayTotal(double bill, double tipPercentage)
+        {
+            double tip = bill * (tipPercentage/100);
+            double billTotal = tip + bill;
+            Console.WriteLine($"Bill: {bill:C}");
+            Console.WriteLine($"Tip: {tipPercentage}%  == {tip:C}");
+            Console.WriteLine($"Total Bill: {billTotal:C}");
+        }
+    }
+}
